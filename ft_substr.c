1b9b1ef08ff0265ba9895620573516f820cb5f96
Alexander Matos <alexgmatosc@gmail.com>
2023-02-06 17:04:57 +0100

    Fixed substr memory overflow

--- ft_substr.c
+++ ft_substr.c
@@ -15,22 +15,24 @@
 char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
 	char	*substr;
+	size_t	str_len;
 	size_t	i;
 	size_t	j;
 
 	i = start;
 	j = 0;
-	if (start > ft_strlen(str))
+	str_len = ft_strlen(str);
+	if (start > str_len)
 	{	
 		return (ft_strdup(""));
 	}
 	else if (!str)
 		return (0);
-	{
-		substr = malloc(len + 1);
-		if (!substr)
-			return (0);
-	}
+	else if (start + len > str_len)
+		len = str_len - start;
+	substr = malloc(len + 1);
+	if (!substr)
+		return (0);
 	while (j < len && str[i])
 	{
 		substr[j] = str[i];
@@ -39,5 +41,4 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 	}
 	substr[j] = 0;
 	return (substr);
-	free(substr);
 }
