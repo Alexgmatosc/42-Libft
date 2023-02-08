d5de2539f1317c31594a8990866913d5e22b5eac
Alexander Matos <alexgmatosc@gmail.com>
2023-02-08 17:17:18 +0100

    Added fd functions

--- libft.h
+++ libft.h
@@ -36,8 +36,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *ptr1, const void *ptr2, size_t n);
-char    *ft_strchr(const char *str, int c);
-char    *ft_strrchr(const char *str, int c);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strnstr(const char *s1, const char *s2, size_t len);
@@ -49,5 +49,8 @@ char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_striteri(char *s, void (*f)(unsigned int, char *));
-
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 #endif
