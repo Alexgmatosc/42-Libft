0fdb400ae0feb2fccb2d027bdff6022784cfc249
Alexander Matos <alexgmatosc@gmail.com>
2023-02-09 19:11:25 +0100

    first itoa attempt

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,14 +1,55 @@
-n = 131231;
- 
-s = -1
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alematos <alematos@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 15:47:57 by alematos          #+#    #+#             */
+/*   Updated: 2023/02/09 19:09:50 by alematos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-p = 1;
-T = 10;
-while (n > T)
-	T = T * 10;
-	p++;
+#include "libft.h"
 
-malloc 8;
-p = 7;
-str[p - 1] (6) = n % 10;
-n = n / 10
+static	int	ft_digit_count(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		count++;
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+	int		sign;
+
+	len = ft_digit_count(n);
+	sign = 1;
+	if (n < 0)
+		sign = -1;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	while (len >= 0)
+	{
+		str[len - 1] = (n % 10) * sign + '0';
+		n /= 10;
+		len--;
+	}
+	if (sign == -1)
+		str[0] = '-';
+	return (str);
+}
