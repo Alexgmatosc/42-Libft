2c6a13b41b89dfdc8d2e42a7f74dc20aa78bd4ac
Alexander Matos <alematos@c1r16s4.42madrid.com>
2023-02-13 20:03:46 +0100

    fixed itoa <3

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: alematos <alematos@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:47:57 by alematos          #+#    #+#             */
-/*   Updated: 2023/02/09 19:09:50 by alematos         ###   ########.fr       */
+/*   Updated: 2023/02/13 20:03:15 by alematos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,18 +36,22 @@ char	*ft_itoa(int n)
 	int		sign;
 
 	len = ft_digit_count(n);
+ 	printf("len = %d\n", len);
 	sign = 1;
 	if (n < 0)
 		sign = -1;
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
+	printf("origen = %p\n", str);
 	str[len] = '\0';
-	while (len >= 0)
+	printf("NULL = %p\n", str + len);
+	while (len > 0)
 	{
 		str[len - 1] = (n % 10) * sign + '0';
 		n /= 10;
 		len--;
+		printf("iter = %p\n", str + len);
 	}
 	if (sign == -1)
 		str[0] = '-';
